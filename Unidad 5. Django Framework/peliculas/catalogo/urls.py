8da9641131e673a7f6ab7bc0dffdc1c825618c72
Fabio Pinto <ingfabiopin@gmail.com>
2021-11-25 18:42:14 -0500

    'Clase 25 de noviembre'

--- Unidad 5. Django Framework/peliculas/catalogo/urls.py
+++ Unidad 5. Django Framework/peliculas/catalogo/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_movies.as_view(), name = 'lista'),
-    path('nuevo/', views.create_movie.as_view(), name = 'crear')
+    path('prueba', views.Prueba.as_view(), name = 'prueba'), #localhost/catalogo/prueba
+    path('listado', views.listarMovies.as_view(), name = 'lista'),
+    path('action', views.listarGenero.as_view(), name = 'action'),
+    path('listaporgenero', views.listaPorBusqueda.as_view(), name = 'busqueda'),
+    path('buscarnombre', views.buscarNombre.as_view(), name = 'busqueda'),
+    path('detalle/<pk>', views.detallePeli.as_view(), name = 'detalle'),
+    path('crear', views.crearPelicula.as_view(), name = 'crear'),
+    path('delete/<pk>', views.eliminarPelicula.as_view(), name = 'eliminar'),
+    path('update/<pk>', views.updatePelicula.as_view(), name = 'actualizar')
 ]
--- Unidad 5. Django Framework/peliculas/catalogo/views.py
+++ Unidad 5. Django Framework/peliculas/catalogo/views.py
@@ -5,37 +5,89 @@ from django.urls import reverse_lazy
 from .models import Movies
 
 from django.views.generic import (
-    ListView, CreateView, )
+    TemplateView, ListView, DetailView, CreateView, DeleteView, UpdateView)
 
-from .forms import PeliculaForm
+class Prueba(TemplateView):
+    template_name = 'prueba.html'
 
-# Creando una nueva pelicula
-class create_movie(CreateView):
-    model = Movies
-    template_name = 'nueva_pelicula.html'
-    form_class = PeliculaForm
-    success_url = reverse_lazy('lista')
+# Vistas para hacer listado - ListView
+## Listado general
+# Listar por genero (que yo busque un genero y me de los que sonde ese genero)
+# Busqueda
 
-# Create your views here.
-# vista de lista - List View
-class list_movies(ListView):
-    template_name = 'catalogo.html'
+class listaNumero(ListView):
+    template_name = 'lista1.html'
+    queryset = list(range(1,10))
     context_object_name = 'pelicula'
 
+
+class listarMovies(ListView):
+    template_name = 'catalogo.html'
+    model = Movies
+    context_object_name = 'pelicula'
+
+class listarGenero(ListView):
+    template_name = 'peliculaGenero.html'
+    model = Movies
+    queryset = Movies.objects.filter(
+        genero = 'Infantil'
+    )
+    context_object_name = 'action'
+
+class listarGeneroUrl(ListView):
+    template_name = 'peliculaGenero2.html'
+
     def get_queryset(self):
-        key_word = self.request.GET.get('nombre', '')
-        _list = Movies.objects.filter(
-            nombre__icontains = key_word
+       gen = self.kwargs['genero']
+       list = Movies.objects.filter(
+           genero = gen
+       )
+       return list
+
+    context_object_name = 'list'
+
+class listaPorBusqueda(ListView):
+    template_name = 'peliculaGenero2.html'
+    context_object_name = 'peli'
+
+    def get_queryset(self):
+        busqueda = self.request.GET.get('genero', '')
+        lista = Movies.objects.filter(
+            genero=busqueda
         )
-        return _list
-# vista de agregar
+        return lista
 
+class buscarNombre(ListView):
+    template_name = 'peliculaGenero3.html'
+    context_object_name = 'peli'
 
+    def get_queryset(self):
+        busqueda = self.request.GET.get('nombre', '')
+        lista = Movies.objects.filter(
+            nombre__contains=busqueda
+        )
+        return lista
 
+class detallePeli(DetailView):
+    template_name = "detalle.html"
+    model = Movies
+    context_object_name = 'peli'
 
-# vista de editar
+class crearPelicula(CreateView):
+    model = Movies
+    template_name = 'crear.html'
+    fields = ['idPeli', 'nombre', 'genero'] # Lista
+    success_url = 'listado' #Path de la url
 
+class eliminarPelicula(DeleteView):
+    model = Movies
+    template_name = 'delete.html'
+    context_object_name = 'pelicula'
+    success_url = reverse_lazy('lista') #nombre de la url
 
-
-# vista de eliminar
-
+class updatePelicula(UpdateView):
+    model = Movies
+    template_name = 'update.html'
+    fields = ('__all__')     #global
+    context_object_name = 'pelicula'
+    success_url = reverse_lazy('lista')
