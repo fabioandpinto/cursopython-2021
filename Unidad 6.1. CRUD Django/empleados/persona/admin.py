56ed8b6d7667e0479be68787d1ae6098b23bfa38
Fabio Pinto <ingfabiopin@gmail.com>
2022-02-20 20:42:51 -0500

    'NUEVO PROYECTO - DJANGO + APIS'

--- Unidad 6.1. CRUD Django/empleados/persona/admin.py
+++ Unidad 6.1. CRUD Django/empleados/persona/admin.py
@@ -12,15 +12,11 @@ class PersonaAdmin(admin.ModelAdmin):
         'position',
         'full_name'
     )
-
     # funcion para armar el full name
     def full_name(self,obj):
         fullname = obj.first_name + ' ' + obj.last_name
         return fullname
 
     list_filter = ('departament', 'position')
-
-
-
 
 admin.site.register(Persona, PersonaAdmin)
